4a47894711409b32303b116fee228cf9b706d9bc
Jack Yu <jackyu4508@gmail.com>
2025-11-16 13:56:51 -0600

    fixed quantization quality report imports

--- benchmark/quantization_benchmark/__init__.py
+++ benchmark/quantization_benchmark/__init__.py
@@ -7,13 +7,11 @@ Includes performance metrics, quality analysis, and visualization tools.
 
 __version__ = "1.0.0"
 
-from .benchmark import run_benchmark
 from .analysis import analyze_performance
 from .quality_eval import evaluate_quantization_quality
 from .visualization import create_visualizations
 
 __all__ = [
-    'run_benchmark',
     'analyze_performance',
     'evaluate_quantization_quality',
     'create_visualizations'
